add73e6680aa50a08929708d1129f4b8c4b6b673
Derick Hansraj <Derickhansraj@ymail.com>
2022-09-19 19:27:45 -0400

    updated server

--- index.js
+++ index.js
@@ -103,7 +103,7 @@ app.post("/create-customer-portal-session", cors(), async (req, res) => {
     console.log(cid);
     const session = await stripe.billingPortal.sessions.create({
       customer: cid,
-      return_url: "https://washgolaundry.netlify.app/",
+      return_url: "https://lpnyc.netlify.app/",
     });
 
     //res.redirect(session.url);
